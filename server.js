4e0a465d2e8c05e01175b4d8be289abd8822bc38
Weston Ruter <westonruter@google.com>
2022-10-24 18:27:34 -0700

    Initial working partytown atomics

---
+++ server.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const fs = require("fs");
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // set this to true for detailed logging:
+  logger: false,
+});
+
+// See <https://partytown.builder.io/atomics#document-response-headers>.
+const crossOriginIsolationHeaders = {
+  "Cross-Origin-Embedder-Policy": "credentialless",
+  "Cross-Origin-Opener-Policy": "same-origin",
+};
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/",
+  setHeaders(res) {
+    for (const [key, value] of Object.entries(crossOriginIsolationHeaders)) {
+      res.setHeader(key, value);
+    }
+  },
+});
+
+// point-of-view is a templating manager for fastify
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
+
+const partytownInlineScript = fs.readFileSync(
+  path.join(__dirname, "public/~partytown/partytown.js")
+);
+
+fastify.get("/", function (request, reply) {
+  reply.headers(crossOriginIsolationHeaders);
+  reply.view("/src/index.hbs", {
+    partytown_inline_script: partytownInlineScript,
+  });
+});
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
+  }
+);
